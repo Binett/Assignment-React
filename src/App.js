3fa8bace916bf50b5e24c97185eb7e1010bfe8d1
Tobias Binett <tobias_binett@hotmail.se>
2021-09-15 13:56:27 +0200

    Added some styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes } from "./routes/Routes";
 import { NavigationDesktop } from "./components/navigation/navigationdesktop/NavigationDesktop";
+import './shared/global/Global.css'
 
 
 function App() {
