3800998023ce7c9e5bcf0ce2b35f24fe89537d9d
Tobias Binett <tobias_binett@hotmail.se>
2021-09-20 10:20:02 +0200

    Added resultcarlogic

--- src/components/showcard/ResultCard.js
+++ src/components/showcard/ResultCard.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useContext } from "react";
+import { ApiContext } from "../../shared/provider/ApiContext";
 
 export const ResultCard = () => {
-    return (
-        <div>
-            
+  const { data } = useContext(ApiContext);
+
+  return (
+    <div className="result card">
+      {data.length > 0}(
+      {data.map((shows) => (
+        <div key={shows.id}>
+          <h3>{shows.name}</h3>
+          <img src={shows.image?.medium} alt="Movie Poster" />
+          <h4>{shows.premiered ? shows.premiered.substring(0, 4) : "-"}</h4>
         </div>
-    )
-}
+      ))}      
+    </div>
+  );
+};
--- src/views/favouritesview/FavouritesView.js
+++ src/views/favouritesview/FavouritesView.js
@@ -1,11 +1,13 @@
 import { useLocation } from "react-router-dom"
+import { ResultCard } from "../../components/showcard/ResultCard"
 
 export const FavouritesView = () => {
     const location = useLocation()
 
     return (
         <div>            
-            {(location.state) ? <h1>{location.state}</h1> : <h1>Shows Page</h1> } 
+            {(location.state) ? <h1>{location.state}</h1> : <h1>Shows Page</h1>}
+            <ResultCard/>
         </div>
     )
 }
