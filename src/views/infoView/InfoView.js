15e12332cb536ca4c9200a849f5db00137407709
Tobias Binett <tobias_binett@hotmail.se>
2021-09-22 18:39:51 +0200

    fixed a bug in infoView

--- src/views/infoView/InfoView.js
+++ src/views/infoView/InfoView.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useLocation,useHistory } from 'react-router'
+import RoutingPath from '../../routes/RoutingPath'
 
 
 export const InfoView = () => {
@@ -7,15 +8,25 @@ export const InfoView = () => {
     const history = useHistory()
     console.log(location.state);
 
+    const displayStateData = () =>{
+        return location.state ? (
+            <div>
+                <h1>{location.state.name}</h1>
+                <h3>{location.state.name}</h3>
+                <img src={location.state.image?.original} alt="Movie Poster" />
+                <h4>{location.state.premiered ? location.state.premiered.substring(0, 4) : "-"}</h4>
+                <h2>{location.state.summary.replace(/<[^&>]*>/g, ' ')}</h2>
+                <button onClick={()=>history.goBack()} >Go Back</button>
+            </div>
+        ) : (
+            <div>{history.push(RoutingPath.homeView)}</div>
+        )
+    }
+
 
     return (
         <div>
-            <h1>{location.state.name}</h1>
-            <h3>{location.state.name}</h3>
-            <img src={location.state.image?.original} alt="Movie Poster" />
-            <h4>{location.state.premiered ? location.state.premiered.substring(0, 4) : "-"}</h4>
-            <h2>{location.state.summary.replace(/<[^&>]*>/g, ' ')}</h2>
-            <button onClick={()=>history.goBack()} >Go Back</button>
+            {displayStateData()}
         </div>
     )
 }
