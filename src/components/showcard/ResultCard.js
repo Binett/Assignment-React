a161920000bea551bfe1b745c83b5da4c6ad85cf
Tobias Binett <tobias_binett@hotmail.se>
2021-09-26 16:29:10 +0200

    Refactoring resultcard for scalability

--- src/components/showcard/ResultCard.js
+++ src/components/showcard/ResultCard.js
@@ -1,50 +1,28 @@
-import { useContext, useState } from "react";
-import { ApiContext } from "../../shared/provider/ApiContext";
-import { Pagination } from "../pagination/Pagination";
-import { useHistory,useLocation } from "react-router";
 import RoutingPath from "../../routes/RoutingPath";
-import loader from '../../shared/images/loader.gif'
+import { useHistory } from "react-router";
 import classes from "./ResultCard.module.css";
 
-export const ResultCard = () => {
+export const ResultCard = ({ shows,currentPage }) => {
   const history = useHistory();
-  const location = useLocation()
-  const { data,isLoading,error} = useContext(ApiContext);
-  const [currentPage, setCurrentPage] = useState(location.state >1 ? location.state : 1);
-  const [postsPerPage] = useState(8);
-
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPost = data.slice(indexOfFirstPost, indexOfLastPost);
-
-
-  const paginate = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
-
   return (
     <div>
-       {isLoading ? <img src={loader} alt="Loading bar"/> : error && <p>Something went wrong, please try again!</p>}
-      <div className={classes.result__card}>
-        {currentPost.length > 0}
-        {currentPost.map((shows) => (
-          <div key={shows.id} className={classes.post}>
-            <h3>{shows.name}</h3>
-            <img src={shows.image?.medium} alt="Movie Poster" />
-            <h4>{shows.premiered ? shows.premiered.substring(0, 4) : "-"}</h4>
-            <button onClick={() => history.push(RoutingPath.infoView, {shows: shows, currentPage: currentPage})}>
-              Show More info
-            </button>
-          </div>
-        ))}
+  
+        <div key={shows.id} className={classes.post}>
+          <h3>{shows.name}</h3>
+          <img src={shows.image?.medium} alt="Movie Poster" />
+          <h4>{shows.premiered ? shows.premiered.substring(0, 4) : "-"}</h4>
+          <button
+            onClick={() =>
+              history.push(RoutingPath.infoView, {
+                shows: shows,
+                currentPage: currentPage,
+              })
+            }
+          >
+            Show More info
+          </button>
+        </div>
       </div>
-      <div>
-        <Pagination
-          postPerPage={postsPerPage}
-          totalPost={data.length}
-          paginate={paginate}
-        />
-      </div>
-    </div>
+    
   );
 };
--- src/views/showsview/ShowsView.js
+++ src/views/showsview/ShowsView.js
@@ -1,14 +1,51 @@
-import { useLocation } from "react-router-dom"
-import { ResultCard } from "../../components/showcard/ResultCard"
-import './ShowsView.css'
+import { useContext, useState } from "react";
+import { ApiContext } from "../../shared/provider/ApiContext";
+import { useLocation } from "react-router-dom";
+import { ResultCard } from "../../components/showcard/ResultCard";
+import { Pagination } from "../../components/pagination/Pagination";
+import loader from "../../shared/images/loader.gif";
+import classes from "./ShowsView.module.css";
 
 export const ShowsView = () => {
-    const location = useLocation()
-    return (
-        <div>            
-             <h1>Shows Page</h1>
-            {(location.state) && <h4>{location.state.user}</h4>}
-            <ResultCard />
-        </div>
-    )
-}
+  const location = useLocation();
+  const { data, isLoading, error } = useContext(ApiContext);
+  const [currentPage, setCurrentPage] = useState(
+    location.state > 1 ? location.state : 1
+  );
+  const [postsPerPage] = useState(8);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPost = data.slice(indexOfFirstPost, indexOfLastPost);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  return (
+    <div>
+      {isLoading ? (
+        <img src={loader} alt="Loading bar" />
+      ) : (
+        error && <p>Something went wrong, please try again!</p>
+      )}
+      <h1>Shows Page</h1>
+      {location.state && <h4>{location.state.user}</h4>}
+      <div className={classes.post}>
+        {currentPost.length > 0}
+        {currentPost.map((shows) => (
+          <div key={shows.id}>
+            <ResultCard shows={shows} currentPage={currentPage} />
+          </div>
+        ))}
+      </div>
+      <div>
+        <Pagination
+          postPerPage={postsPerPage}
+          totalPost={data.length}
+          paginate={paginate}
+        />
+      </div>
+    </div>
+  );
+};
